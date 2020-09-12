fe127b992bc551c803a86a7dc23dc8f468e7161d
Zengit <edvinsmineikis@gmail.com>
2020-09-12 22:06:31 +0300

    Update arpdos.py

--- arpdos.py
+++ arpdos.py
@@ -14,7 +14,8 @@ for i in sys.argv:
 	elif i == '-exclude':
 		mode = 'exclude'
 
-router = os.popen('sudo route | grep "default" | awk \'{print($2)}\'').read().strip('\n') #get routers ip string
+router = os.popen('sudo route | grep "default" | awk \'{print($2)}\'').read().strip('\n')
+
 print('Default gateway: '+router)
 
 if mode == 'exclude':
@@ -23,15 +24,11 @@ if mode == 'exclude':
 	for i in range(len(sys.argv)-indx-1):
 		targets.append(sys.argv[i+indx+1])
 else:
-	ifconfig = os.popen('sudo ifconfig wlan0 | grep "broadcast"').read() #get the line for machines ip and subnet
-
-	machine = ifconfig.split(' ')[9] #get devices ip string
-
-	subnet = ifconfig.split(' ')[12] #get subnet mask string
-
+	ifconfig = os.popen('sudo ifconfig wlan0 | grep "broadcast"').read()
+	machine = ifconfig.split(' ')[9]
+	subnet = ifconfig.split(' ')[12]
 	print('\nThis device is '+machine+' under the subnet of '+subnet)
-
-	if subnet == '255.0.0.0': #create a subnet CIDR number: /8 /16 /24
+	if subnet == '255.0.0.0':
 		cidr = '8'
 	elif subnet == '255.255.0.0':
 		cidr = '16'
@@ -40,33 +37,27 @@ else:
 	else:
 		print('Weird subnet, cannot work with that..')
 		exit()
-
 	input('\nPress enter to start [nmap -sn '+router+'/'+cidr+']   ')
-
-	#Do nmap scan, grep IPs of alive hosts and make a list of them, remove the last '' which is not needed
 	targets = os.popen('sudo nmap -sn '+router+'/'+cidr+' | grep "Nmap scan report for" | awk \'{print($5)}\'').read().split('\n')[:-1]
-
-	for i in range(len(targets)): #get rid of routers ip in the list
+	for i in range(len(targets)):
 		if targets[i] == router:
 			targets.pop(i)
 			break
-
-	for i in range(len(targets)): #get rid of machines ip in the list
+	for i in range(len(targets)):
 		if targets[i] == machine:
 		        targets.pop(i) 
 		        break
-
 	if mode == 'add':
 		indx = sys.argv.index('-add')
 		for i in range(len(sys.argv)-indx-1):
 			if sys.argv[i+indx+1] not in targets:
 				targets.append(sys.argv[i+indx+1])
 
-if type(targets) is not list: #if the list is empty, its type will be 'None', so no targets are found
+if type(targets) is not list:
 	print('\nNo targets found, ARPDOS cannot be done.')
 	exit()
 else:
-	print('\nTargets ('+str(len(targets))+'):') #print alive hosts in a better looking manner
+	print('\nTargets ('+str(len(targets))+'):')
 	for i in targets:
 		print(i)
 
